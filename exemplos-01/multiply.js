69f1f2b07f04ab6f4a280df25ee577539e71950c
Junior <jjalves96@gmail.com>
2020-10-01 07:59:52 -0300

    feat: first test

---
+++ exemplos-01/multiply.js
@@ -0,0 +1,4 @@
+export function multiply(number1, number2) {
+
+  return number1 * number2;
+}
---
+++ exemplos-01/multiply.spec.js
@@ -0,0 +1,5 @@
+import { multiply } from './multiply';
+
+it('should multiply 2 and 2 the result must be 4', () => {
+  expect(multiply(2, 2)).toBe(4);
+});
