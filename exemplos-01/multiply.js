9b0382fb0a143da70ddeb1910c68c4b1cb29eb68
Junior <jjalves96@gmail.com>
2020-10-01 08:10:43 -0300

    feat: add multiply and tests

--- exemplos-01/multiply.js
+++ exemplos-01/multiply.js
@@ -1,4 +1,10 @@
 export function multiply(number1, number2) {
+  const int1 = parseInt(number1);
+  const int2 = parseInt(number2);
 
-  return number1 * number2;
+  if(Number.isNaN(int1) || Number.isNaN(int2)) {
+    throw new Error('Please, check your input.');
+  }
+
+  return int1 * int2;
 }
