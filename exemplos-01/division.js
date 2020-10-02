e4b13ab031cb66867ffaa78b2011f12eb589c9b2
Junior <jjalves96@gmail.com>
2020-10-02 08:24:17 -0300

    feat: add division and first test

---
+++ exemplos-01/division.js
@@ -0,0 +1,6 @@
+export function division(int1, int2) {
+  const number1 = parseInt(int1);
+  const number2 = parseInt(int2);
+
+  return number1 / number2;
+}
---
+++ exemplos-01/division.spec.js
@@ -0,0 +1,5 @@
+import { division } from './division';
+
+it('should division 2 and 2 the result must be 1', () => {
+  expect(division(2, 2)).toBe(1);
+});
