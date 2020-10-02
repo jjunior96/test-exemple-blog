e8f7185650a199be8d191822b5027a10d232ad6d
Junior <jjalves96@gmail.com>
2020-10-02 08:28:27 -0300

    chore: add test for division by 0

--- exemplos-01/division.js
+++ exemplos-01/division.js
@@ -2,5 +2,9 @@ export function division(int1, int2) {
   const number1 = parseInt(int1);
   const number2 = parseInt(int2);
 
+  if(number2 === 0) {
+    throw new Error('Please, not allow division by 0');
+  }
+
   return number1 / number2;
 }
--- exemplos-01/division.spec.js
+++ exemplos-01/division.spec.js
@@ -3,3 +3,13 @@ import { division } from './division';
 it('should division 2 and 2 the result must be 1', () => {
   expect(division(2, 2)).toBe(1);
 });
+
+it('should division 2 and 2 even if on of them is a string and the result must be 1', () => {
+  expect(division('2', 2)).toBe(1);
+});
+
+it('should trow an error if the second params is 0', () => {
+  expect(() => {
+    division(2, 0)
+  }).toThrowError();
+});
